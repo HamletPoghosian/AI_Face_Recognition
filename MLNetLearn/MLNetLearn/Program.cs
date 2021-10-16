8cedb2e7b308cbaaba2e65de1652c83ae3d1d8f5
Hamlet Poghosyan <phamik@mail.ru>
2021-10-16 21:21:54 +0400

    some changes

---
+++ MLNetLearn/MLNetLearn/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ML;
+using System;
+
+namespace MLNetLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var contex = new MLContext()
+            {
+                
+            };
+
+        }
+    }
+}
