6764a14a3a16a03b450f524adbdfcde5ded81108
Hamlet Poghosyan <hamlet.poghosyan@volo.global>
2021-12-02 12:23:48 +0400

    add new file  for training

--- Python_ML_AI/ML/Dynamic_Mappings/djnamic_j2j.py
+++ Python_ML_AI/ML/Dynamic_Mappings/djnamic_j2j.py
@@ -10,7 +10,7 @@ import csv
 
 
 # show model
-mapping_data = pd.read_csv('dynamic_mapping_ml.csv')
+mapping_data = pd.read_csv('mapping_for_ai_test.csv')
 data = mapping_data['Key_in']
 values = array(data)
 label_encoder = preprocessing.LabelEncoder()
@@ -19,23 +19,19 @@ mapping_data['keyinLabel'] = label_encoder.fit_transform(values)
 
 X = mapping_data.drop(columns=['Key_in','key_out'])
 y = mapping_data['key_out']
-# mapping_data['keyinLabelValue'] = label_encoder.inverse_transform(mapping_data['keyinLabel'] )
+mapping_data['keyinLabelValue'] = label_encoder.inverse_transform(mapping_data['keyinLabel'] )
+# print(mapping_data)
 
 keyin_input = input()
 keyin_type = eval(input())
 predictiondata=''
 
-iter = -1
-myData = pd.read_csv("mapping_base.csv")
-for row in myData['key_in']:
-    iter +=1
-    if row == keyin_input:
-        predictiondata = myData['key_in_label'][iter]
+
 
 # step 1 , we have already trained model , so every next mapping should convert keyinLabel .write in csv and use everytime
 # with open('mapping_base.csv', 'a',newline='') as f:
 #     writer = csv.writer(f)
-#     header =['key_in', 'key_in_label']
+#     header =['Key_in', 'key_in_label']
 #     writer.writerow(header)
 #     kk=-1
 #     for row in mapping_data['Key_in']:
@@ -45,6 +41,13 @@ for row in myData['key_in']:
 #     f.close()
 
 
+
+iter = -1
+myData = pd.read_csv("mapping_base.csv")
+for row in myData['Key_in']:
+    iter +=1
+    if row == keyin_input:
+        predictiondata = myData['key_in_label'][iter]
 
 
 # encoding value to set into algortim , string cant use in ML , only Integer ,
@@ -63,12 +66,12 @@ for row in myData['key_in']:
 
 
 # train ML and test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-model = DecisionTreeClassifier()
-model.fit(X_train.values, y_train.values)
-predictions = model.predict(X_test)
-score = accuracy_score(y_test.values, predictions)
-print('accuracy of predicition :', score * 10 , 'of 10')
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# model = DecisionTreeClassifier()
+# model.fit(X_train.values, y_train.values)
+# predictions = model.predict(X_test)
+# score = accuracy_score(y_test.values, predictions)
+# print('accuracy of predicition :', score * 10 , 'of 10')
 
 # prediction based on model
 # model = DecisionTreeClassifier()
@@ -85,13 +88,18 @@ model = joblib.load('mapping-recommender.joblib')
 predictions = model.predict([[keyin_type,predictiondata]])
 print('KeyOut will be :',predictions[0])
 
+print('KeyOut will be :',predictions[0])
+print('KeyOut will be :',predictions[0])
+
 # encoded to find keyIn property name
 keyin_name = label_encoder.inverse_transform([predictiondata])
 print('KeyIn will be :',keyin_name[0])
 
 
 print('Type will be :',keyin_type)
+print('-\n\n\n\n\---------Result-------\-')
 
+print('\n {{ \n"keyIn": "{0}",\n"keyOut": "{1}",\n"value": null,\n"type": {2},\n"format": null,\n"keyOutValidation": null,\n"functions": [],\n"childs": []\n }}'.format(keyin_name[0],predictions[0],keyin_type))
 
 # [7,5] means  type = 7   keyinLabel = 5 so keyout will be Items , keyin will be items
 
